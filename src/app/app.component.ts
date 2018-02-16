760d5282712376b2732f80630fe2b59b961f7be0
Rodrigo Muniz <rodrigo@mamangava.com>
2018-02-16 15:49:21 -0200

    learnangular5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  query: string;
+  artists: object;
+  currentArtist : object;
+
+  showArtist = function (item) {
+    console.log(item);
+    this.query = item.name;
+    item.highlight = !item.highlight;
+    this.currentArtist = item;
+  }
+
+  constructor(private http: HttpClient) {
+    this.query = '';
+  }
+
+  ngOnInit(): void {
+    this.http.get<Object>('../assets/data.json').subscribe(
+      data => {
+        this.artists = data;
+      }
+    )
+  }
+
 }
